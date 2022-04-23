670cac4a073063e273915045a9b60b947530eac1
kennedymutisya <kenmsh@gmail.com>
2022-04-23 12:20:52 +0300

    feat: Configure api Authentication to use JWT tokens

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,28 @@
 <?php
 
+use App\Http\Controllers\Jwt\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group([
+
+        'middleware' => 'api',
+        'prefix'     => 'v1',
+
+], function ($router) {
+
+    Route::group(['prefix' => 'user'], function () {
+
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('me', [AuthController::class, 'me']);
+
+    });
+    Route::post('register', [AuthController::class, 'register']);
+
+
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
