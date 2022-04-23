6e90b75bcc37d775ea8908526ba61e858e52a685
kennedymutisya <kenmsh@gmail.com>
2022-04-23 14:06:16 +0300

    feat: Add an inverse relationship to Post model to get the author/user of the post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+            'created_at' => 'datetime:Y-m-d H:i:s',
+            'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
