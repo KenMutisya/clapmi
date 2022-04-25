6be92545c5045eeaa3dac298bdf301de4e60841d
kennedymutisya <kenmsh@gmail.com>
2022-04-25 10:25:45 +0300

    fix: Add Category API Resource

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin \App\Models\Category */
+class CategoryResource extends JsonResource
+{
+    /**
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+                'id'         => $this->id,
+                'name'       => $this->name,
+                'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+                'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+        ];
+    }
+}
