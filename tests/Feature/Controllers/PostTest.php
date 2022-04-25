e289fc2f94016c18a4a549a26276fc065514f1d0
kennedymutisya <kenmsh@gmail.com>
2022-04-25 08:44:09 +0300

    test: Add tests for creating,updating and deleting a post

--- tests/Feature/Controllers/PostTest.php
+++ tests/Feature/Controllers/PostTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Events\PostCreatedEvent;
 use App\Models\Enums\Status;
 use App\Models\Post;
 use App\Models\User;
@@ -12,7 +13,7 @@ use Tests\TestCase;
 
 class PostTest extends TestCase
 {
-    use RefreshDatabase,WithFaker;
+    use RefreshDatabase, WithFaker;
 
     /** @test */
     public function it_can_list_all_posts_for_a_user()
@@ -51,7 +52,6 @@ class PostTest extends TestCase
         $response->assertJsonCount(3, 'data');
     }
 
-
     public function userSetup()
     {
         $user = User::factory()->create([
@@ -64,5 +64,119 @@ class PostTest extends TestCase
         ]);
 
         return [$user, $token->json('access_token')];
+    }
+
+    /** @test */
+    public function it_can_create_a_post_for_a_user()
+    {
+        \Event::fake();
+
+        [$user, $token] = $this->userSetup();
+
+
+        $response = $this->postJson(route('posts.store'), [
+                'title'    => 'Test Post',
+                'category' => 'Test Category',
+                'status'   => Status::PUBLISHED->value,
+
+        ]);
+
+        $response->assertJsonStructure([
+                'data' => [
+                        'id',
+                        'title',
+                        'category',
+                        'user',
+                        'status',
+                        'created_at',
+                        'updated_at',
+                ],
+        ]);
+
+        \Event::assertDispatched(PostCreatedEvent::class);
+    }
+
+
+    /** @test */
+    public function it_can_update_a_post_you_own()
+    {
+
+        [$user, $token] = $this->userSetup();
+
+        $post = Post::factory()->create([
+                'title'    => 'Old Title',
+                'category' => 'Old Category',
+                'status'   => Status::DRAFT->value,
+                'user_id'  => $user->id,
+        ]);
+
+        $this->assertSame('Old Title', $post->title);
+        $this->assertSame('Old Category', $post->category);
+        $this->assertSame(Status::DRAFT->value, $post->status);
+
+        $response = $this->patchJson(route('posts.update', $post->id), [
+                'title'    => 'Updated Post',
+                'category' => 'New Category',
+                'status'   => Status::PUBLISHED->value,
+
+        ]);
+
+        $post->refresh();
+
+
+        $this->assertSame('Updated Post', $post->title);
+        $this->assertSame('New Category', $post->category);
+        $this->assertSame(Status::PUBLISHED->value, $post->status);
+
+
+    }
+
+    /** @test */
+    public function it_cannot_update_a_post_you_dont_own()
+    {
+
+        [$user, $token] = $this->userSetup();
+
+        $post = Post::factory()->create([
+                'title'    => 'Old Title',
+                'category' => 'Old Category',
+                'status'   => Status::DRAFT->value,
+        ]);
+
+        $response = $this->patchJson(route('posts.update', $post->id), [
+                'title'    => 'Updated Post',
+                'category' => 'New Category',
+                'status'   => Status::PUBLISHED->value,
+
+        ]);
+
+        $this->assertSame('You cannot update this post.',$response->json('message'));
+
+    }
+
+    /** @test */
+    public function it_can_delete_a_post()
+    {
+
+        [$user, $token] = $this->userSetup();
+
+        $post = Post::factory()->create([
+                'user_id' => $user->id,
+        ]);
+
+        $response = $this->deleteJson(route('posts.destroy', $post->id));
+        $this->assertSame('Post deleted successfully', $response->json('message'));
+    }
+
+    /** @test */
+    public function it_cannot_delete_a_post_you_dont_own()
+    {
+        [$user, $token] = $this->userSetup();
+
+        $post = Post::factory()->create();
+
+        $response = $this->deleteJson(route('posts.destroy', $post->id));
+
+        $this->assertSame('You can only Delete your own posts.', $response->json('message'));
     }
 }
